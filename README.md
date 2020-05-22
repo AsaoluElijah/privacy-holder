e7540d7cd700f7a84699a314779c76546b578eda
ihavecoke <ihavecoke@163.com>
2020-05-22 23:57:34 +0800

    Add basic function.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # privacy-holder
-privacy you email, mobile, id_card and name with placeholder * :)
+
+Hidden Privacy String with * 
+
+## How to use ?
+
+### email 
+
+    input: ihavecoke@163.com
+    output: ih*******@163.com
+     
+### phone number
+
+    input: 13800000000
+    output: 138****0000
---
+++ index.test.js
@@ -0,0 +1,28 @@
+const privacy = require('./index.js');
+
+
+test('mobile', () => {
+  expect(privacy.mobile('13800000000')).toEqual('138****0000');
+  expect(privacy.mobile('+86-13800000000')).toEqual('+86-138****0000')
+});
+
+test('email', () => {
+  expect(privacy.email('a@163.com')).toEqual('*@163.com');
+  expect(privacy.email('ab@163.com')).toEqual('**@163.com');
+  expect(privacy.email('abc@163.com')).toEqual('ab*@163.com');
+  expect(privacy.email('ihavecoke@163.com')).toEqual('ih*******@163.com');
+});
+
+test('idCard', () => {
+  expect(privacy.idCard('510122199102132018')).toEqual('510***19******2018')
+});
+
+test('name', () => {
+  expect(privacy.name('ihavecoke')).toEqual('*');
+  expect(privacy.name('i havecoke')).toEqual('*havecoke');
+  expect(privacy.name('i have coke')).toEqual('*coke')
+});
+
+test('all', () => {
+  expect(privacy.all('any-string-will-replace-with-*')).toEqual('******************************');
+});
